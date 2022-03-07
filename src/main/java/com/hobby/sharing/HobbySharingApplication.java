9a74f887ead0f0005e54ecaa4c1b884d286cb3a0
wkddnjsgh <luca4759@gmail.com>
2022-03-07 16:45:13 +0900

    Initial commit

---
+++ src/main/java/com/hobby/sharing/HobbySharingApplication.java
@@ -0,0 +1,13 @@
+package com.hobby.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbySharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbySharingApplication.class, args);
+    }
+
+}
