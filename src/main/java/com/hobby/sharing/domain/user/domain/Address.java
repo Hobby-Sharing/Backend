4f55f9c1879e24c7ec5f56d4ac0e6e577255f29d
wkddnjsgh <luca4759@gmail.com>
2022-03-15 09:24:06 +0900

    📑 :: 주소 엔티티 작성

---
+++ src/main/java/com/hobby/sharing/domain/user/domain/Address.java
@@ -0,0 +1,20 @@
+package com.hobby.sharing.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Address implements Serializable {
+
+    private String roadNameAddress;
+    private int zipCode;
+
+}
