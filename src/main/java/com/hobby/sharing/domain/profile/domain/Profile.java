5a824fa9dc96e12e547f336331e0c52ed933178f
wkddnjsgh <luca4759@gmail.com>
2022-03-15 09:24:43 +0900

    📑 :: 프로필 엔티티 작성

---
+++ src/main/java/com/hobby/sharing/domain/profile/domain/Profile.java
@@ -0,0 +1,34 @@
+package com.hobby.sharing.domain.profile.domain;
+
+import com.hobby.sharing.domain.model.BaseTimeEntity;
+import com.hobby.sharing.domain.user.domain.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Profile extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(50) default 'no profile image", nullable = false)
+    private String profileImageUrl;
+
+    @Column(length = 30, nullable = false)
+    private String statusMessage;
+
+    @Builder
+    public Profile(User user, String profileImageUrl, String statusMessage) {
+        this.user = user;
+        this.profileImageUrl = profileImageUrl;
+        this.statusMessage = statusMessage;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
