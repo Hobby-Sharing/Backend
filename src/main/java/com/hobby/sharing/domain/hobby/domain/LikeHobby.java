51559af06e2008b354b201c845231458462c165e
wkddnjsgh <luca4759@gmail.com>
2022-03-15 09:25:48 +0900

    📑 :: 관심 취미 엔티티 작성

---
+++ src/main/java/com/hobby/sharing/domain/hobby/domain/LikeHobby.java
@@ -0,0 +1,26 @@
+package com.hobby.sharing.domain.hobby.domain;
+
+import com.hobby.sharing.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class LikeHobby {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hobby_id")
+    private Hobby hobby;
+}
