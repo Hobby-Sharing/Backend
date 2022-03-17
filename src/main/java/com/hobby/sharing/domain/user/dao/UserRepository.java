d13ae9e63ec97fdc5f9c6494f1ab31e718e1bbd3
wkddnjsgh <luca4759@gmail.com>
2022-03-17 14:07:42 +0900

    ⚡ :: findByEmail 추가

--- src/main/java/com/hobby/sharing/domain/user/dao/UserRepository.java
+++ src/main/java/com/hobby/sharing/domain/user/dao/UserRepository.java
@@ -3,5 +3,10 @@ package com.hobby.sharing.domain.user.dao;
 import com.hobby.sharing.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
