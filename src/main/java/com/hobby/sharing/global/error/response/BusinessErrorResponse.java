083add7764837392d3f4747b4f801710197fd842
wkddnjsgh <luca4759@gmail.com>
2022-03-17 20:00:17 +0900

    ⚡ :: ErrorResponse의 LocalDataTime을 ZonedDateTime으로 변경

--- src/main/java/com/hobby/sharing/global/error/response/BusinessErrorResponse.java
+++ src/main/java/com/hobby/sharing/global/error/response/BusinessErrorResponse.java
@@ -3,12 +3,13 @@ package com.hobby.sharing.global.error.response;
 import com.hobby.sharing.global.error.ErrorCode;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Getter
 public class BusinessErrorResponse {
 
-    private final LocalDateTime localDateTime;
+    private final ZonedDateTime zonedDateTime;
 
     private final int status;
 
@@ -17,11 +18,9 @@ public class BusinessErrorResponse {
     private final String message;
 
     public BusinessErrorResponse(ErrorCode errorCode) {
-        this.localDateTime = LocalDateTime.now();
+        this.zonedDateTime = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
         this.status = errorCode.getStatus();
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
     }
-}
-
 }
