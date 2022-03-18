fb278bd75284d15ea0ded7bd3f8b69c853224088
wkddnjsgh <luca4759@gmail.com>
2022-03-18 12:01:25 +0900

    📑 :: JwtProperties 작성

---
+++ src/main/java/com/hobby/sharing/global/security/jwt/JwtProperties.java
@@ -0,0 +1,25 @@
+package com.hobby.sharing.global.security.jwt;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.util.Base64;
+
+@Getter
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private final Long accessExp;
+    private final Long refreshExp;
+    private final String secretKey;
+
+
+    public JwtProperties(String secretKey, Long accessExp, Long refreshExp) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        this.accessExp = accessExp;
+        this.refreshExp = refreshExp;
+    }
+
+}
