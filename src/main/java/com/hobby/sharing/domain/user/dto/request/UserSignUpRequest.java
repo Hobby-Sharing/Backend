c19afe8ec63a1dbc6bbcc1e818413206dc94175a
wkddnjsgh <luca4759@gmail.com>
2022-03-28 23:03:16 +0900

    ⚡ :: UserSignUpRequest validation 수정

--- src/main/java/com/hobby/sharing/domain/user/dto/request/UserSignUpRequest.java
+++ src/main/java/com/hobby/sharing/domain/user/dto/request/UserSignUpRequest.java
@@ -3,17 +3,14 @@ package com.hobby.sharing.domain.user.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Getter
 @NoArgsConstructor
 public class UserSignUpRequest {
 
     @NotBlank(message = "이메일은 비어 있을 수 없습니다.")
-    @Pattern(regexp = "^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$",
-            message = "올바른 이메일 형식이 아닙니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 비어 있을 수 없습니다.")
@@ -21,9 +18,15 @@ public class UserSignUpRequest {
             message = "비밀번호는 적어도 하나의 숫자와 소문자를 포함한 8글자 이상이어야 하며, 공백이 포함될 수 없습니다.")
     private String password;
 
-    @Size(min = 0, max = 8, message = "이름은 8글자 이하여야 합니다.")
+    @NotBlank(message = "이름을 입력해주세요.")
+    @Size(max = 8, message = "이름은 8글자 이하여야 합니다.")
     private String name;
 
-    private int zipCode;
+    @NotNull(message = "우편번호를 입력해주세요.")
+    @Min(value = 9999, message = "우편번호 5자리가 올바르게 입력되었는지 확인해주세요.")
+    @Digits(integer = 5, fraction = 0, message = "우편번호 5자리가 올바르게 입력되었는지 확인해주세요.")
+    private Integer zipCode;
+
+    @NotBlank(message = "도로명주소를 입력해주세요.")
     private String roadNameAddress;
 }
