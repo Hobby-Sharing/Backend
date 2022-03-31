b1cc3c04e5648b5756d8b20760e947add103595c
wkddnjsgh <luca4759@gmail.com>
2022-03-31 19:24:40 +0900

    ⚡ :: 필요없는 인자 제거

--- src/main/java/com/hobby/sharing/domain/profile/application/ProfileDetailsInfoService.java
+++ src/main/java/com/hobby/sharing/domain/profile/application/ProfileDetailsInfoService.java
@@ -2,7 +2,6 @@ package com.hobby.sharing.domain.profile.application;
 
 import com.hobby.sharing.domain.profile.dao.CustomProfileRepository;
 import com.hobby.sharing.domain.profile.dto.response.ProfileDetailsInfoResponse;
-import com.hobby.sharing.global.security.facade.AuthFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProfileDetailsInfoService {
 
-    private final AuthFacade authFacade;
     private final CustomProfileRepository customProfileRepository;
 
     public ProfileDetailsInfoResponse execute() {
-        String email = authFacade.getUserEmail();
-        return customProfileRepository.getProfileDetailsInfo(email);
+        return customProfileRepository.getProfileDetailsInfo();
     }
 }
