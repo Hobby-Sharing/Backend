6c3b43dd2fafa1e3f0888d2c8447af068eeb966b
wkddnjsgh <luca4759@gmail.com>
2022-04-01 09:19:08 +0900

    📑 :: ProfileManagementController 작성 후 통합

--- src/main/java/com/hobby/sharing/domain/profile/api/ProfileManagementController.java
+++ src/main/java/com/hobby/sharing/domain/profile/api/ProfileManagementController.java
@@ -1,9 +1,12 @@
 package com.hobby.sharing.domain.profile.api;
 
+import com.hobby.sharing.domain.profile.application.ProfileDetailsInfoService;
 import com.hobby.sharing.domain.profile.application.ProfileRegistrationService;
 import com.hobby.sharing.domain.profile.dto.request.ProfileRegistrationRequest;
+import com.hobby.sharing.domain.profile.dto.response.ProfileDetailsInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,15 @@ import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
-public class ProfileRegistrationController {
+public class ProfileManagementController {
 
+    private final ProfileDetailsInfoService profileDetailsInfoService;
     private final ProfileRegistrationService profileRegistrationService;
+
+    @GetMapping("/profile") @PreAuthorize("isAuthenticated()")
+    public ProfileDetailsInfoResponse getProfileDetails() {
+        return profileDetailsInfoService.execute();
+    }
 
     @PostMapping("/profile") @PreAuthorize("isAuthenticated()")
     public void profileRegistration(@Valid @RequestBody ProfileRegistrationRequest request) {
