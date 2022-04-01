b40e5eb162af11eb2e889b05cbb8a1373e6f54f9
wkddnjsgh <luca4759@gmail.com>
2022-04-01 09:41:21 +0900

    ⚡ :: 사용자 정보를 가져오는 메소드 추가

--- src/main/java/com/hobby/sharing/global/security/facade/AuthFacade.java
+++ src/main/java/com/hobby/sharing/global/security/facade/AuthFacade.java
@@ -1,14 +1,26 @@
 package com.hobby.sharing.global.security.facade;
 
+import com.hobby.sharing.domain.user.dao.UserRepository;
+import com.hobby.sharing.domain.user.domain.User;
+import com.hobby.sharing.domain.user.exception.UserNotFoundException;
 import com.hobby.sharing.global.security.auth.AuthDetails;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class AuthFacade {
 
-    public String getUserEmail() {
+    private final UserRepository userRepository;
+
+    public User getUser() {
+        String email = getUserEmail();
+        return userRepository.findByEmail(email).orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    private String getUserEmail() {
         return ((AuthDetails)getAuthentication().getPrincipal()).getUsername();
     }
 
