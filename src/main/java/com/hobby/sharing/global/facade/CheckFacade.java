a4b8204657d184cc5a803e18684d6d14350bff95
wkddnjsgh <luca4759@gmail.com>
2022-04-01 20:00:42 +0900

    ⚡ :: UserFacade -> CheckFacade 메소드 추가

--- src/main/java/com/hobby/sharing/global/facade/CheckFacade.java
+++ src/main/java/com/hobby/sharing/global/facade/CheckFacade.java
@@ -1,7 +1,10 @@
-package com.hobby.sharing.domain.user.facade;
+package com.hobby.sharing.global.facade;
 
+import com.hobby.sharing.domain.profile.dao.ProfileRepository;
+import com.hobby.sharing.domain.profile.exception.ProfileAlreadyExistsException;
 import com.hobby.sharing.domain.user.dao.CustomUserRepository;
 import com.hobby.sharing.domain.user.dao.UserRepository;
+import com.hobby.sharing.domain.user.domain.User;
 import com.hobby.sharing.domain.user.exception.PasswordMismatchException;
 import com.hobby.sharing.domain.user.exception.UserAlreadyExistsException;
 import com.hobby.sharing.domain.user.exception.UserNotFoundException;
@@ -11,15 +14,22 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class UserFacade {
+public class CheckFacade {
 
     private final CustomUserRepository customUserRepository;
     private final UserRepository userRepository;
+    private final ProfileRepository profileRepository;
     private final PasswordEncoder passwordEncoder;
 
     public void checkUserDuplicate(String email) {
         if (userRepository.existsByEmail(email)) {
             throw UserAlreadyExistsException.EXCEPTION;
+        }
+    }
+
+    public void checkProfileDuplicate(User user ) {
+        if (profileRepository.existsByUser(user)) {
+            throw ProfileAlreadyExistsException.EXCEPTION;
         }
     }
 
