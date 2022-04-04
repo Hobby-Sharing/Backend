3dafc0952325af169282333d1dc4d7cfcd27c0aa
wkddnjsgh <luca4759@gmail.com>
2022-04-04 09:13:05 +0900

    ⚡ :: JPA Auditing이 정상적으로 적용되지 않았던 문제 수정

--- src/main/java/com/hobby/sharing/global/config/JpaAuditingConfig.java
+++ src/main/java/com/hobby/sharing/global/config/JpaAuditingConfig.java
@@ -8,7 +8,7 @@ import java.util.TimeZone;
 
 @Configuration
 @EnableJpaAuditing
-public abstract class JpaAuditingConfig {
+public class JpaAuditingConfig {
 
     @PostConstruct
     private void started() {
