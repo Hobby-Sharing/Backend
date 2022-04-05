1b526d76fdab06a1b592adddcc85c75f988995ab
wkddnjsgh <luca4759@gmail.com>
2022-04-05 19:23:12 +0900

    📑 :: HobbyNotFoundException 작성

---
+++ src/main/java/com/hobby/sharing/domain/hobby/exception/HobbyNotFoundException.java
@@ -0,0 +1,13 @@
+package com.hobby.sharing.domain.hobby.exception;
+
+import com.hobby.sharing.global.error.ErrorCode;
+import com.hobby.sharing.global.error.exception.BasicException;
+
+public class HobbyNotFoundException extends BasicException {
+
+    public final static HobbyNotFoundException EXCEPTION = new HobbyNotFoundException();
+
+    private HobbyNotFoundException(){
+        super(ErrorCode.HOBBY_NOT_FOUND);
+    }
+}
--- src/main/java/com/hobby/sharing/global/error/ErrorCode.java
+++ src/main/java/com/hobby/sharing/global/error/ErrorCode.java
@@ -10,10 +10,13 @@ public enum ErrorCode {
     EXPIRED_TOKEN(401, "AUTH-401-1", "Expired Token"),
     INVALID_TOKEN(401, "AUTH-401-2", "Invalid Token"),
     PASSWORD_MISMATCH(401, "AUTH-401-3", "The Password incorrect"),
-    USER_NOT_FOUND(404, "USER-404-1", "User Not Found"),
 
-    USER_ALREADY_EXISTS(409, "USER-409-1", "This email is already registered."),
-    PROFILE_ALREADY_EXISTS(409, "PROFILE-409-1", "This is a registered profile."),
+    USER_NOT_FOUND(404, "USER-404-1", "User Not Found"),
+    HOBBY_NOT_FOUND(404, "HOBBY-404-1", "Hobby Not Found"),
+
+    USER_ALREADY_EXISTS(409, "USER-409-1", "This email is already registered"),
+    PROFILE_ALREADY_EXISTS(409, "PROFILE-409-1", "This is a registered profile"),
+
     ADDRESS_SEARCH_FAILED(500, "SERVER-500-1", "Address Search Failed");
 
     private final int status;
