e825e33eec8890f8d81d0d31bf0e9a28eccf4870
wkddnjsgh <luca4759@gmail.com>
2022-04-05 16:47:08 +0900

    ⚡ :: 취미 목록 조회 시 정렬 기능 추가

--- src/main/java/com/hobby/sharing/domain/hobby/dao/HobbyRepository.java
+++ src/main/java/com/hobby/sharing/domain/hobby/dao/HobbyRepository.java
@@ -1,10 +1,11 @@
 package com.hobby.sharing.domain.hobby.dao;
 
 import com.hobby.sharing.domain.hobby.domain.Hobby;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface HobbyRepository extends CrudRepository<Hobby, Long> {
-    List<Hobby> findAll();
+    List<Hobby> findAll(Sort sort);
 }
