fc0555845f491d56c217bba28044acbdd768850d
wkddnjsgh <luca4759@gmail.com>
2022-04-08 10:59:29 +0900

    ⚡:: Service 이름 변경

--- src/main/java/com/hobby/sharing/domain/hobby/application/SelectAllHobbyService.java
+++ src/main/java/com/hobby/sharing/domain/hobby/application/SelectAllHobbyService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-public class HobbyListService {
+public class SelectAllHobbyService {
 
     private final HobbyRepository hobbyRepository;
 
