ff3bf0c0bacaf00d60d055d9f56ac37e8725e3af
wkddnjsgh <luca4759@gmail.com>
2022-04-11 09:22:32 +0900

    ⚡:: 모든 취미 조회 API에 취미 ID 추가

--- src/main/java/com/hobby/sharing/domain/hobby/dto/response/HobbyListResponse.java
+++ src/main/java/com/hobby/sharing/domain/hobby/dto/response/HobbyListResponse.java
@@ -4,13 +4,16 @@ import com.hobby.sharing.domain.hobby.domain.Hobby;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 @AllArgsConstructor
 public class HobbyListResponse {
+    private UUID hobbyId;
     private String name;
     private String categoryName;
 
     public static HobbyListResponse from(Hobby hobby) {
-        return new HobbyListResponse(hobby.getName(), hobby.getCategory().getName());
+        return new HobbyListResponse(hobby.getId(), hobby.getName(), hobby.getCategory().getName());
     }
 }
