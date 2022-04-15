66fe6cf1b108d02d31a4690b4db345941b79b644
wkddnjsgh <luca4759@gmail.com>
2022-04-15 11:28:05 +0900

    ⚡ :: 취미 이름으로 검색 API에 정렬 기능 추가

--- src/main/java/com/hobby/sharing/domain/hobby/application/SearchHobbyNameService.java
+++ src/main/java/com/hobby/sharing/domain/hobby/application/SearchHobbyNameService.java
@@ -3,6 +3,7 @@ package com.hobby.sharing.domain.hobby.application;
 import com.hobby.sharing.domain.hobby.dao.HobbyRepository;
 import com.hobby.sharing.domain.hobby.dto.response.HobbyResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +17,7 @@ public class SearchHobbyNameService {
 
     @Transactional(readOnly = true)
     public List<HobbyResponse> execute(String keyword) {
-        return hobbyRepository.findByNameContains(keyword)
+        return hobbyRepository.findByNameContains(keyword, Sort.by(Sort.Direction.ASC, "hobbyName"))
                 .stream()
                 .map(HobbyResponse::from)
                 .toList();
--- src/main/java/com/hobby/sharing/domain/hobby/dao/HobbyRepository.java
+++ src/main/java/com/hobby/sharing/domain/hobby/dao/HobbyRepository.java
@@ -1,11 +1,12 @@
 package com.hobby.sharing.domain.hobby.dao;
 
 import com.hobby.sharing.domain.hobby.domain.Hobby;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface HobbyRepository extends CrudRepository<Hobby, UUID> {
-    List<Hobby> findByNameContains(String keyword);
+    List<Hobby> findByNameContains(String keyword, Sort sort);
 }
