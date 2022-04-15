cf1e420bc356a770028a71a9a3482b33020af877
wkddnjsgh <luca4759@gmail.com>
2022-04-15 11:51:01 +0900

    ⚡ :: 복합 키 비교를 위한 어노테이션 추가

--- src/main/java/com/hobby/sharing/domain/club/domain/embed/ClubEmbed.java
+++ src/main/java/com/hobby/sharing/domain/club/domain/embed/ClubEmbed.java
@@ -2,12 +2,14 @@ package com.hobby.sharing.domain.club.domain.embed;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
